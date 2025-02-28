0c5377b162353be3adaeec15126d3d34c195ac16
Felipe Lima <felipe-fsl@live.com>
2025-02-28 16:39:54 -0300

    Commit inicial

---
+++ typescript.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+const resultadoMultiplicacao = multiplicar(5, 3);
+const mensagemSaudacao = saudacao("João");
+console.log(resultadoMultiplicacao);
+console.log(mensagemSaudacao);
